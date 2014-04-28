93315881df4fbabf233cea060f5f49baa986ab18
William D. Meldon <wdmeldon@gmail.com>
2014-04-28 16:06:28 -0400

    Phantom needed the fully qualified name for some reason.

--- tests/unit/models/todo-test.js
+++ tests/unit/models/todo-test.js
@@ -11,7 +11,7 @@ test("it exists", function() {
 test('artificial promise thing', function () {
   return Ember.run(function(){
     // TODO:  https://github.com/emberjs/ember.js/pull/4176
-    return new Promise(function(resolve) {
+    return new Ember.RSVP.Promise(function(resolve) {
       Ember.run.later(function(){
 
         ok(true, "no really, seems good");
