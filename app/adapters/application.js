343d3defdba384fc7e327bbc4cb3a927316d45f7
Stefan Penner <stefan.penner@gmail.com>
2015-03-24 20:49:17 -0700

    remove fixture adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,3 +1,49 @@
 import DS from 'ember-data';
 
-export default DS.FixtureAdapter.extend({ });
+export default DS.Adapter.extend({
+  findAll() {
+    // rather then doing an ajax, just echo back the default data
+
+    return [
+      {
+        id: '1',
+        title: 'install ember-cli',
+        isCompleted: true
+      },
+      {
+        id: '2',
+        title: 'install additional dependencies',
+        isCompleted: true
+      },
+      {
+        id: '3',
+        title: 'develop amazing things',
+        isCompleted: false
+      }
+    ];
+  },
+
+  createRecord(store, type, record) {
+    // rather then doing an ajax, just echo back the data that was created
+    var json = record.toJSON();
+
+    // assign a unique ID like the server word
+    json.id = Date.now();
+
+    // return a value or a promise
+    return json;
+  },
+
+  updateRecord(store, type, record) {
+    // rather then doing an ajax, just echo back the data that was updated
+    var json = record.toJSON();
+
+    json.id = record.id;
+
+    return json;
+  },
+
+  deleteRecord(store, type, record) {
+    return record;
+  }
+});
--- app/models/todo.js
+++ app/models/todo.js
@@ -1,26 +1,6 @@
-// models/todo.js
 import DS from 'ember-data';
 
-var Todo = DS.Model.extend({
+export default DS.Model.extend({
   title: DS.attr('string'),
   isCompleted: DS.attr('boolean')
 });
-
-Todo.reopenClass({
-  FIXTURES: [
-  {
-    id: "1",
-    title: 'install ember-cli',
-    isCompleted: true
-  }, {
-    id: "2",
-    title: 'install additional dependencies',
-    isCompleted: true
-  }, {
-    id: "3",
-    title: 'develop amazing things',
-    isCompleted: false
-  }
-]});
-
-export default Todo;
