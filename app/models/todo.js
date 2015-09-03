66213a322cbcdb48b1c531ee96ddd661e13d6d13
Matthew Rudy Jacobs <matthewrudyjacobs@gmail.com>
2015-09-03 18:35:26 +0800

    Use the extracted ember-data-fixture-adapter

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,6 +1,28 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+var Todo = DS.Model.extend({
   title: DS.attr('string'),
   isCompleted: DS.attr('boolean', { defaultValue: false })
 });
+
+Todo.reopenClass({
+  FIXTURES: [
+    {
+      id: '1',
+      title: 'install ember-cli',
+      isCompleted: true
+    },
+    {
+      id: '2',
+      title: 'install additional dependencies',
+      isCompleted: true
+    },
+    {
+      id: '3',
+      title: 'develop amazing things',
+      isCompleted: false
+    }
+  ]
+});
+
+export default Todo;
