60ea95b47fd05a697d680cc554ebf4176625cc60
Matthew Rudy Jacobs <matthewrudyjacobs@gmail.com>
2015-09-03 17:52:35 +0800

    Update the test find methods to use findAll and findRecord

--- tests/integration/models/todo-test.js
+++ tests/integration/models/todo-test.js
@@ -16,19 +16,24 @@ test('contrived example, loading an additional todo', function(assert) {
   // he user interacts with the application (via click or something)
   // so lets simulate that via an programmatic run-loop (normally the eventDispatcher does this for us)
   return Ember.run(() => {
-    return store.find('todo').then((todos) => {
+    return store.findAll('todo').then((todos) => {
 
       // ensure new length
       var numberOfTodos = todos.get('length');
 
       // lets pretend another Todo was added
-      store.push('todo', {
-        id: '9999',
-        title: 'install EAK',
-        isCompleted: true
+      store.push({
+        data: {
+          type: 'todo',
+          id: '9999',
+          attributes: {
+            title: 'install EAK',
+            isCompleted: true
+          }
+        }
       });
 
-      store.find('todo', 9999).then((todo) => {
+      store.findRecord('todo', 9999).then((todo) => {
         // some what trivial but still a good test
         assert.equal('9999',        todo.get('id'));
         assert.equal('install EAK', todo.get('title'));
@@ -36,7 +41,7 @@ test('contrived example, loading an additional todo', function(assert) {
       });
 
       // lets do another findAll
-      return store.find('todo').then((todos) => {
+      return store.findAll('todo').then((todos) => {
         assert.equal(numberOfTodos + 1, todos.get('length'), 'expect an additional todo');
       });
     });
