3d718b6589c53e8d315f5ca0c4736ee7a6ca234c
Benjamin Fleischer <benjamin.fleischer@swipesense.com>
2015-10-28 09:44:09 -0500

    Configure bootstrap-sass
    recommends autoprefixer
    https://github.com/twbs/bootstrap-sass#sass-autoprefixer

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,8 +3,17 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var options = {
+    sassOptions: {
+      includePaths: [
+        'bower_components/bootstrap-sass/assets/stylesheets'
+      ]
+    }
   };
   var app = new EmberApp(defaults, options);
+
+  // tooltips, popovers
+  // app.import('bower_components/bootstrap-sass/assets/javascripts/bootstrap.js');
+  app.import('bower_components/bootstrap-sass/assets/stylesheets/_bootstrap.scss');
 
   return app.toTree();
 };
