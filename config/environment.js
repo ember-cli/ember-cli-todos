edce955a85a3385b720a5b28040e94bd8d77930b
Benjamin Fleischer <benjamin.fleischer@swipesense.com>
2015-10-28 04:26:42 -0500

    Clean up

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'todos',
+    modulePrefix: 'frontend',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
