51ee55b62630438b2f0f3fb8282c92d1ce883302
Vic Boudolf <vic@comdirections.com>
2017-02-23 10:45:29 -0500

    add code that can be uncommented to toggle from using the stubbed API to a real API
    update readme

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,1 +1,7 @@
+// comment out the next line to make the app access the elixir REST API
 export { default } from 'ember-data-fixture-adapter';
+
+// uncomment the next lines to make the app access the elixir REST API
+// import JSONAPIAdapter from 'ember-data/adapters/json-api';
+// export default JSONAPIAdapter.extend({
+// });
