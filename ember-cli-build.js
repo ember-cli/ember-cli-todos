13fca38966267472353eefc8d3eff16811719fd8
Martin Broerse <broerse@martinic.com>
2017-03-28 11:38:02 +0200

    Upgrade to ember-cli 2.12.0

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,5 +1,5 @@
-/* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+/* eslint-env node */
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
@@ -18,7 +18,6 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('bower_components/moment/moment.js');
 
   return app.toTree();
 };
